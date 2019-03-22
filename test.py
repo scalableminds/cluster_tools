1dd7e1c459a8098008cc08df3549208e4ed1420d
Philipp Otto <philipp.4096@googlemail.com>
2019-03-22 10:17:33 +0100

    brush up tests and change cwd before executing tests

--- test.py
+++ test.py
@@ -2,60 +2,55 @@ import cluster_tools
 import subprocess
 import concurrent.futures
 import time
+import sys
 
 # "Worker" functions.
 def square(n):
     return n * n
 
-def hostinfo():
-    return subprocess.check_output('uname -a', shell=True)
-
 def sleep(duration):
     time.sleep(duration)
-    return True
+    return duration
 
 
-def test_submit():
-    """Square some numbers on remote hosts!
-    """
-
-    def square_numbers(executor):
-        with executor:
-            job_count = 5
-            job_range = range(job_count)
-            print("submitting job")
-            futures = [executor.submit(square, n) for n in job_range]
-            for future, job_index in zip(futures, job_range):
-                assert future.result() == square(job_index)
-
-    executors = [
+def get_executors():
+    return [
         cluster_tools.get_executor("slurm", debug=True, keep_logs=True),
         cluster_tools.get_executor("multiprocessing", 5),
         cluster_tools.get_executor("sequential")
     ]
-    for exc in executors:
-        square_numbers(exc)
+
+def test_submit():
+    def run_square_numbers(executor):
+        with executor:
+            job_count = 5
+            job_range = range(job_count)
+            futures = [executor.submit(square, n) for n in job_range]
+            for future, job_index in zip(futures, job_range):
+                assert future.result() == square(job_index)
+
+    for exc in get_executors():
+        run_square_numbers(exc)
 
 
-def example_2():
+def test_unordered_sleep():
     """Get host identifying information about the servers running
     our jobs.
     """
-    with cluster_tools.SlurmExecutor(False) as executor:
-        futures = [executor.submit(hostinfo) for n in range(15)]
-        for future in concurrent.futures.as_completed(futures):
-            print(future.result().strip())
+    def run_sleeps(executor):
+        with executor:
+            durations = [10, 5, 15]
+            futures = [executor.submit(sleep, n) for n in durations]
+            durations.sort()
+            for duration, future in zip(durations, concurrent.futures.as_completed(futures)):
+                assert future.result() == duration
+
+    for exc in get_executors():
+        run_sleeps(exc)
 
 
-def example_3():
-    with cluster_tools.SlurmExecutor(False) as exc:
-        print(list(exc.map(square, [5, 7, 11, 12, 13, 14, 15, 16, 17])))
-
-
-def sleep_example():
-    executor = cluster_tools.get_executor("slurm")
-    # executor = cluster_tools.get_executor("multiprocessing", 5)
-    # executor = cluster_tools.get_executor("sequential")
-    with executor:
-        print(list(executor.map(sleep, [10, 10, 10])))
-
+def test_map():
+    def run_map(executor):
+        with executor:
+            result = list(executor.map(square, [2, 3, 4]))
+            assert result == [4, 9, 16]
