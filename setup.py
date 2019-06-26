7cd0576adaf760c7fc56951960049c78a10b0756
Philipp Otto <philipp.4096@googlemail.com>
2019-06-26 13:52:53 +0200

    update setup.py version to 1.25-dev-2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def _read(fn):
     return open(path).read()
 
 setup(name='cluster_tools',
-      version='1.25-dev',
+      version='1.25-dev-2',
       description='Utility library for easily distributing code execution on clusters',
       author='scalableminds',
       author_email='hello@scalableminds.com',
