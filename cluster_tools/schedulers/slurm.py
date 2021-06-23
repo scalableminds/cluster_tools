30ba3f85523d5d5c17b3c667fd82a1d79f3b9951
Daniel Werner <daniel.werner@scalableminds.com>
2021-06-23 18:38:04 +0200

    add comments to variables that need to be kept in sync and explain sbatch placeholders

--- cluster_tools/schedulers/slurm.py
+++ cluster_tools/schedulers/slurm.py
@@ -59,10 +59,13 @@ class SlurmExecutor(ClusterExecutor):
         job_id = cls.get_current_job_id()
         job_array_id = cls.get_job_array_id()
         job_array_index = cls.get_job_array_index()
-        return cls.format_log_file_path(
-            cfut_dir,
-            job_id if job_array_index is None else f"{job_array_id}_{job_array_index}",
+
+        # This variable needs to be kept in sync with the job_id_string variable in the
+        # inner_submit function.
+        job_id_string = (
+            job_id if job_array_index is None else f"{job_array_id}_{job_array_index}"
         )
+        return cls.format_log_file_path(cfut_dir, job_id_string)
 
     def submit_text(self, job):
         """Submits a Slurm job represented as a job file string. Returns
@@ -86,9 +89,11 @@ class SlurmExecutor(ClusterExecutor):
         """Starts a Slurm job that runs the specified shell command line.
         """
 
-        log_path = self.format_log_file_path(
-            self.cfut_dir, "%j" if job_count is None else "%A_%a"
-        )
+        # These place holders will be replaced by sbatch, see https://slurm.schedmd.com/sbatch.html#SECTION_%3CB%3Efilename-pattern%3C/B%3E
+        # This variable needs to be kept in sync with the job_id_string variable in the
+        # get_log_file_path function.
+        job_id_string = "%j" if job_count is None else "%A_%a"
+        log_path = self.format_log_file_path(self.cfut_dir, job_id_string)
 
         job_resources_lines = []
         if self.job_resources is not None:
